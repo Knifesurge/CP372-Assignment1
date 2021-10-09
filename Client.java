608562e32451dd5f33e43deba6bf9843aa9d1ee6
Knifesurge <carlos94563@gmail.com>
2021-10-08 23:47:50 -0400

    Moved Client code into methods to be called by ClientGUI, made Client a SwingWorker to allow concurrent execution with Swing components

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,17 +15,50 @@ import java.net.UnknownHostException;
  *
  * Connects to Server, sends requests to Server, displays responses.
  */
-public class Client implements Runnable {
+public class Client extends SwingWorker {
 
     private static Socket client_socket;
+    private static boolean connected = false;
     private PrintWriter output;
     private BufferedReader input;
     private BufferedReader stdIn;
     private String serverAddr;
     private int serverPort;
+    private static JTextArea textArea;
 
-    public Client() {
+    public Client(JTextArea textArea) {
+        this.textArea = textArea;
+    }
 
+    @Override
+    protected String doInBackground() {
+        String serverResp = "";  // Messages from server
+        try {
+            while ((serverResp = input.readLine()) != null) {
+                //if(UserCommand.equals("DISCONNECT")) break;
+                System.out.println(serverResp);
+                textArea.append("Server: " + serverResp + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return serverResp;
+    }
+
+    public void sendMessage(String[] message, boolean argsRequired) {
+        StringBuilder outMsg = new StringBuilder();
+        if (!argsRequired) {
+            // Command takes no arguments, just send the command
+            outMsg.append(message[0]);
+        } else {
+            // Send the arguments off as well
+            for (String s : message) {
+                outMsg.append(s + "|");
+            }
+        }
+        System.out.println("Sending message: " + outMsg.toString());
+        output.println(outMsg.toString());  // Sends message to Server socket
+        textArea.append("Client: " + outMsg.toString() + "\n");
     }
 
     public void connect(String ip, int port) {
@@ -35,11 +69,13 @@ public class Client implements Runnable {
             output = new PrintWriter(client_socket.getOutputStream(), true);
             input = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
             stdIn = new BufferedReader(new InputStreamReader(System.in));
+            connected = true;
         } catch (UnknownHostException e) {
             System.err.println("Unknown Host, please retry!");
         } catch (IOException e) {
             System.err.println("Unable to read/write, please try again!");
         }
+        System.out.println("Connected to: " + ip + ":" + port);
     }
 
     public void close() {
@@ -51,22 +87,5 @@ public class Client implements Runnable {
             System.err.println("Error closing in/out streams or socket!");
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void run() {
-        String UserCommand;
-        //UserCommand = stdIn.readLine();
-        try {
-            while ((UserCommand = stdIn.readLine()) != null) {
-                //if(UserCommand.equals("DISCONNECT")) break;
-                output.println(UserCommand);
-                System.out.println(input.readLine());
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        close();
     }
 }
