05722608f6203f2d2837c31ee35c873ef4b4b30d
Knifesurge <carlos94563@gmail.com>
2021-10-09 01:52:04 -0400

    Okay, NOW it should handle properly, since it was giving lambda errors

--- Board.java
+++ Board.java
@@ -216,16 +216,6 @@ public class Board {
         boolean fContains = contains.isEmpty() ? false : true;
         boolean fRefersTo = refersTo.isEmpty() ? false : true;
 
-        int x = -1;
-        int y = -1;
-
-        // Get the coords out of the string if present
-        if (fContains) {
-            String[] containsTmp = contains.split(" ");
-            x = Integer.parseInt(containsTmp[0]);
-            y = Integer.parseInt(containsTmp[1]);
-        }
-
         // Checks which filters are present and filters the notes accordingly.
         // The filtered Notes are then added to the fnotes List above, to be returned to the caller.
         // Any suggestions on how to do this without this big branch would be welcomed :)
@@ -235,6 +225,10 @@ public class Board {
         } else {
             if (fColor) {
                 if (fContains) {
+                    // Get the coords out of the string
+                    String[] containsTmp = contains.split(" ");
+                    int x = Integer.parseInt(containsTmp[0]);
+                    int y = Integer.parseInt(containsTmp[1]);
                     if (fRefersTo) {
                         fnotes.addAll(
                                 notes.stream()
